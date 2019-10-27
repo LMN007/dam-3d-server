3d3c21277b1e5138383baf972d9f24993303f4c1
leon <linwe2012@163.com>
2019-10-27 22:03:12 +0800

    clean up

--- main.py
+++ main.py
@@ -31,26 +31,19 @@ def get_db():
 def retImage(p):
     return send_from_directory(unzip_path,os.path.join(p,'scene.gltf'),as_attachment=True)
 
-
-
 @app.route("/api/upload/model", methods = ['GET', 'POST'])
 def upload_model():
     try:
         model_info = request.get_json()
-        # print(type(model_info))
         print(model_info['filename'])
         con = databaseInit()
-        url = createURL(con, model_info)
+        # url = createURL(con, model_info)
         message = sendMessage(con, model_info)
-        # print("message: {}".format(message))
-        # with open("./a_simple_pokeball.zip","rb") as f:
-        #     message['model'] = base64.b64encode(f.read())
         model_info,model_name = refine(message)
         print(model_info)
         print(model_name)
         return "{'code':'0',data:{'preview':'" + model_name +  "'}}"
     except Exception as e:
-        print(e)
         return jsonify({'code':1, 'msg': '{}'.format(e)})
     
 
