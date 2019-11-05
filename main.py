880e5e19a7e7928a3ed3a7db88f977871d879d16
otori-bird <3170104111@zju.edu.cn>
2019-11-05 13:12:02 +0800

    delete os.system

--- main.py
+++ main.py
@@ -274,6 +274,5 @@ def image(p):
 
 
 if __name__ == "__main__":
-    os.system("node ./converter/server.js")
     con = databaseInit(True)
     app.run(debug=True, host='0.0.0.0', port='8080', ssl_context=('cert.pem', 'key.pem'))
